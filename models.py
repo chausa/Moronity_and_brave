b9a9add2da8feb86bd7a7efcbcf80fdf2c643295
Dari <hidaridari@gmail.com>
2012-03-15 17:43:10 +0400

    gitignore

--- models.py
+++ models.py
@@ -5,3 +5,6 @@ class blog(models.Model):
 	title = models.CharField(max_length=32)
 	date = models.DateTimeField(auto_now_add= True)
 	text = models.TextField()
+
+	def __unicode__(self):
+		return u'%s' % self.title
